b570bc50275c63691239a55e518aae22c2ba6848
Nadezhda <oleneva96@gmail.com>
2022-08-18 17:47:24 +0300

    added new blocks and styles for them

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
     blogContainer = document.querySelector('.main_blog_container');
+    secondBlogContainer = document.querySelector('.second_blog_container');
+    thirdBlogContainer = document.querySelector('.third_blog_container')
 
     fetch('https://jsonplaceholder.typicode.com/posts')
         .then(response => response.json())
         .then(arr => {
-            let firstBlockElemeents = '';
+            let firstBlockElements = '';
+            let secondBlockElements = '';
+            let thirdBlogElements = '';
+
+
             for (let i = 0; i < 4; i++) {
                 let post = `<div class="post_container">
                     <div class="pic"></div>
                     <div class="wrapper_text">
                         <h2 class="title">${arr[i].title}</h2>
                         <p class="text">${arr[i].body}</p>
-                    </div>
-                    
+                    </div>    
                 </div>`;
 
                 if (i === 0) {
@@ -26,8 +31,53 @@ document.addEventListener('DOMContentLoaded', () => {
                     post = `${post}</div>`;
                 }
 
-                firstBlockElemeents += post;
+                firstBlockElements += post;
             }
-            blogContainer.innerHTML = firstBlockElemeents;
+
+            for (let i = 4; i < 8; i++) {
+                let post = `<div class="mini_post_container">
+                    <div class="pic"></div>
+                    <div class="wrapper_text">
+                        <h2 class="title">${arr[i].title}</h2>
+                        <p class="text">${arr[i].body}</p>
+                    </div>
+                </div>`
+                
+                secondBlockElements += post;
+            }
+
+            for (let i = 9; i < 18; i++) {
+                let post = `<div class="medium_post_container">
+                    <div class="pic"></div>
+                    <div class="wrapper_text">
+                        <h2 class="title">${arr[i].title}</h2>
+                        <p class="text">${arr[i].body}</p>
+                    </div>
+                </div>`
+
+                if (i === 9) {
+                    post = `<div class="linewrapper_medium_post_container">${post}`
+                } else if (i === 11) {
+                    post = `${post}</div>`;
+                } else if (i === 12) {
+                    post = `<div class="linewrapper_medium_post_container">${post}`
+                } else if (i === 14) {
+                    post = `${post}</div>`;
+                } else if (i === 15) {
+                    post = `<div class="linewrapper_medium_post_container">${post}`
+                } else if (i === 17) {
+                    post = `${post}</div>`;
+                }
+
+                thirdBlogElements += post;
+            }
+
+
+
+            
+            blogContainer.innerHTML = firstBlockElements;
+            secondBlogContainer.innerHTML = secondBlockElements;
+            thirdBlogContainer.innerHTML = thirdBlogElements;
+
         })
 });
